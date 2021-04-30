e9e26e3be770700fba1fad6a04d023e2cec2939b
danniel917 <danniel917@hotmail.com>
2021-04-30 18:44:18 -0500

    First Commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+    let button = document.querySelector("#button");
+
+    
+    function getValue(e) {
+        // debugger
+        // let nombre = document.querySelector("#nombre").value;
+        let nombre = document.querySelector("#nombre").value;
+        let telefono = document.querySelector("#telefono").value;
+        let email = document.querySelector("#email").value;
+        
+                
+        if(!nombre || !telefono || !email){
+                // alert("Completa todos los campos!");
+                return; //Parar ejecucion
+            } else {
+                
+                let result = document.querySelector(".respuesta");
+                // result.style.opacity = "1";
+                // result.style.top = "0";
+                result.style.display= "grid";
+                
+                let inputNombre = document.querySelector(".nombreValue");
+                let inputTelefono = document.querySelector(".telValue");
+                let inputEmail = document.querySelector(".emailValue");
+                
+                inputNombre.innerHTML = "Nombre</br>" + nombre;
+                inputTelefono.innerHTML = "Telefono</br>" + telefono;
+                inputEmail.innerHTML = "Correo</br>" + email;
+                e.preventDefault();    
+
+                    }
+                
+}
+
+button.addEventListener("click", getValue);
+
+
+
+
+
+
+
+
+
+// button.addEventListener("click", clearInputs);
+// function clearInputs() {
+//     // document.querySelector("#button").reset();
+//     let inputs = document.querySelectorAll("input");
+//     inputs.forEach((e) => {
+//       e.value = "";
+//     });
+//   }
+
+let cerrar = document.querySelector(".cerrar");
+
+function cerrarR(){
+    let result = document.querySelector(".respuesta");
+    result.style.display = "none";
+    console.log(cerrar);
+}
+
+cerrar.addEventListener("click", cerrarR)
---
+++ script.js
@@ -0,0 +1,13 @@
+let button1 = document.querySelector("#button");
+
+alert("Hola!!!!!");
+
+button1.addEventListener("click", () => {
+    // let name = document.querySelector("#nombre").value;
+    // alert(name);
+
+});
+
+
+
+
